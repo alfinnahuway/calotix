6c232929e8c9ae4082718bfa2fb92aaa2112918b
iqbalherdiansyah <indiqbal4@gmail.com>
2023-07-16 18:59:20 +0700

    fix slider

--- src/components/Highlight.jsx
+++ src/components/Highlight.jsx
@@ -3,34 +3,44 @@ import LiteYouTubeEmbed from "react-lite-youtube-embed";
 import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
 
 const Highlight = () => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-  };
+	const settings = {
+		dots: true,
+		infinite: true,
+		speed: 500,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+	};
 
-  return (
-    <>
-      <section className="w-full">
-        <main className="w-[80%] mx-auto">
-          <Slider {...settings}>
-            <div className="w-full">
-              <LiteYouTubeEmbed
-                id="9qD254rDtPs"
-                title="YouTube video player"
-                activeClass="lyt-activated"
-                iframeClass=""
-                playerClass="lty-playbtn"
-                wrapperClass="yt-lite"
-              />
-            </div>
-          </Slider>
-        </main>
-      </section>
-    </>
-  );
+	return (
+		<>
+			<section className="w-full">
+				<main className="w-[80%] mx-auto">
+					<Slider {...settings}>
+						<div className="w-full">
+							<LiteYouTubeEmbed
+								id="9qD254rDtPs"
+								title="YouTube video player"
+								activeClass="lyt-activated"
+								iframeClass=""
+								playerClass="lty-playbtn"
+								wrapperClass="yt-lite"
+							/>
+						</div>
+						<div className="w-full">
+							<LiteYouTubeEmbed
+								id="9qD254rDtPs"
+								title="YouTube video player"
+								activeClass="lyt-activated"
+								iframeClass=""
+								playerClass="lty-playbtn"
+								wrapperClass="yt-lite"
+							/>
+						</div>
+					</Slider>
+				</main>
+			</section>
+		</>
+	);
 };
 
 export default Highlight;
