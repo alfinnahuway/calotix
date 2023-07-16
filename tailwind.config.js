b6c4e9887663e3906d77f6805ceefb70fd09c77a
iqbalherdiansyah <indiqbal4@gmail.com>
2023-07-16 15:47:49 +0700

    setup react slick

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}"],
+	content: [
+		"./index.html",
+		"./src/**/*.{js,ts,jsx,tsx}",
+		"node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+		"node_modules/slick-carousel/slick/slick.css",
+		"node_modules/slick-carousel/slick/slick-theme.css",
+	],
 	theme: {
 		extend: {},
 	},
