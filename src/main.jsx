084a6675919422c8a1dcbf51051e3de319d0f71d
iqbalherdiansyah <indiqbal4@gmail.com>
2023-07-16 13:57:38 +0700

    remove slick carousel

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import "../node_modules/slick-carousel/slick/slick.css";
-import "../node_modules/slick-carousel/slick/slick-theme.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
