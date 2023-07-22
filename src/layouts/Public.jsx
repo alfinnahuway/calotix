d68e69a7dca70a0813234d6890d8fce297fc1dc1
iqbal herdiansyah <indiqbal4@gmail.com>
2023-07-22 20:51:22 +0700

    added proptypes

--- src/layouts/Public.jsx
+++ src/layouts/Public.jsx
@@ -1,5 +1,7 @@
 import Footerlocal from "../components/Footer";
 import Header from "../components/Header";
+import PropTypes from "prop-types";
+
 const Public = ({ children }) => {
 	return (
 		<main>
