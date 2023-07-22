e13904150e9daa991907bb1573ddb2bb5454ee61
digolstudiocode <dickydigol17@gmail.com>
2023-07-22 22:07:55 +0700

    design warna

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,43 +3,76 @@ import { FontAwesomeIcon as Icon } from "@fortawesome/react-fontawesome";
 import { faUser, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
-	return (
-		<NavbarBase fluid rounded className="!bg-slate-200 sticky top-0 !rounded-none z-[9999]">
-			<NavbarBase.Brand href="https://flowbite-react.com">
-				<img alt="Flowbite React Logo" className="mr-3 h-6 sm:h-9" src="./public/logo.svg" />
-				<span className="self-center whitespace-nowrap text-xl font-semibold text-black">Calo.Tix</span>
-			</NavbarBase.Brand>
-			<div className="flex md:order-2">
-				<div className="hidden md:flex">
-					<Icon icon={faMagnifyingGlass} size="lg" className="mr-4" />
-					<Icon icon={faUser} size="lg" />
-				</div>
-				<NavbarBase.Toggle />
-			</div>
-			<NavbarBase.Collapse>
-				<div className="flex gap-2 md:hidden">
-					<Button outline color="warning" className="flex-1">
-						Daftar
-					</Button>
-					<Button color="warning" className="flex-1">
-						Masuk
-					</Button>
-				</div>
-				<NavbarBase.Link href="#" className="text-base !text-gray-900">
-					Home
-				</NavbarBase.Link>
-				<NavbarBase.Link href="#" className="text-base !text-gray-900">
-					Event
-				</NavbarBase.Link>
-				<NavbarBase.Link href="#" className="text-base !text-gray-900">
-					Schedule
-				</NavbarBase.Link>
-				<NavbarBase.Link href="#" className="text-base !text-gray-900">
-					About us
-				</NavbarBase.Link>
-			</NavbarBase.Collapse>
-		</NavbarBase>
-	);
+  return (
+    <NavbarBase
+      fluid
+      rounded
+      className="!bg-stone-500 sticky top-0 !rounded-none z-[9999]"
+    >
+      <NavbarBase.Brand href="https://flowbite-react.com">
+        <img
+          alt="Flowbite React Logo"
+          className="mr-3 h-6 sm:h-9"
+          src="./public/logo.svg"
+        />
+        <span className="self-center whitespace-nowrap text-xl font-semibold text-white">
+          Calo.Tix
+        </span>
+      </NavbarBase.Brand>
+      <div className="flex md:order-2">
+        <div className="hidden md:flex">
+          <Icon
+            icon={faMagnifyingGlass}
+            size="lg"
+            className="mr-4"
+            color="black"
+          />
+          <Icon icon={faUser} size="lg" color="black" />
+        </div>
+        <NavbarBase.Toggle className="hover:!bg-transparent focus:!ring-0" />
+      </div>
+      <NavbarBase.Collapse className="!pb-2 ">
+        <div className="flex gap-2 md:hidden">
+          <Button
+            color="transparent"
+            className="flex-1 !border-orange-400 border-2 !text-orange-400 hover:!brightness-75 hover:!text-white focus:!ring-0"
+          >
+            Daftar
+          </Button>
+          <Button
+            color=""
+            className="flex-1 !bg-orange-400 hover:!brightness-75 hover:!text-white focus:!ring-0 hover:!border-2 hover:!border-orange-400 border-2 border-transparent"
+          >
+            Masuk
+          </Button>
+        </div>
+        <NavbarBase.Link
+          href="#"
+          className="text-base !text-gray-900 hover:!text-orange-400 hover:!bg-transparent"
+        >
+          Home
+        </NavbarBase.Link>
+        <NavbarBase.Link
+          href="#"
+          className="text-base !text-orange-400 hover:!text-orange-400 hover:!bg-transparent"
+        >
+          Event
+        </NavbarBase.Link>
+        <NavbarBase.Link
+          href="#"
+          className="text-base !text-gray-900 hover:!text-orange-400 hover:!bg-transparent"
+        >
+          Schedule
+        </NavbarBase.Link>
+        <NavbarBase.Link
+          href="#"
+          className="text-base !text-gray-900 hover:!text-orange-400 hover:!bg-transparent"
+        >
+          About us
+        </NavbarBase.Link>
+      </NavbarBase.Collapse>
+    </NavbarBase>
+  );
 };
 
 export default Navbar;
