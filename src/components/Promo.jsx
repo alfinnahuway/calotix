2d9d5b848499fcb5ef5e6ad78307e7e4f24ee042
febrianmulawarman <febrianmulawarman92@gmail.com>
2023-07-22 20:42:20 +0800

    commit

--- src/components/Promo.jsx
+++ src/components/Promo.jsx
@@ -2,8 +2,7 @@ import {useState} from 'react'
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import {images} from './listPromo/listPromo'
 const Promo = () => {
-
-    
+  
     const [currentImage, setCurrentImage] = useState(0);
     const prevImage = () => {
       const isFirstImage = currentImage === 0;
@@ -19,8 +18,8 @@ const Promo = () => {
   return (
     <div>
       <div className="container">
-        <h1 className="font-extrabold mx-auto text-xl mb-2 mt-5">Promo Terbaru</h1>
-        <div style={{ backgroundImage: `url(${images[currentImage].imgUrl})` }} className="mx-auto h-[200px] md:h-[400px] rounded-sm bg-center bg-cover duration-500 relative py-10">
+        <h1 className="font-extrabold mx-auto text-lg mb-1 mt-5">Promo Terbaru</h1>
+        <div style={{backgroundImage: `url(${images[currentImage].imgUrl})` }} className="mx-auto h-[200px] md:h-[400px] rounded-sm bg-center bg-cover duration-500 relative py-10">
           <div className="text-stone-50 absolute top-[50%] hover:bg-black/50 left-2 cursor-pointer">
             <FaChevronLeft onClick={prevImage} size={20} />
           </div>
