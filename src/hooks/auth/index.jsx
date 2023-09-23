72f117eb2a4e4bf758ce9ff5786496797b4d3708
alfinnahuway <alfintomy08@gmail.com>
2023-09-23 21:43:35 +0700

    deleted react-dotenv

--- src/hooks/auth/index.jsx
+++ src/hooks/auth/index.jsx
@@ -2,7 +2,6 @@ import axios from "axios";
 import { createContext, useContext, useMemo, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import validator from "validator";
-import env from "react-dotenv";
 
 const UserContext = createContext();
 
